3da9d34b33b4d25c74bdb674eb6ceca795405291
Maria <maria.altyeva@gmail.com>
2019-09-30 21:54:48 +0800

    maisonzan landing

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+       $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
